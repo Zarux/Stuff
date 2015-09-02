26c7feb625ce809fb99e1b1e6c3bf2d1d8bad04d
Joakim <joakimtangnes@gmail.com>
2015-09-02 20:46:12 +0200

    Fixed path

--- Uni/pipinstall.sh
+++ Uni/pipinstall.sh
@@ -1,4 +1,4 @@
-wget https://bootstrap.pypa.io/get-pip.py; python get-pip.py --user
-printf "\nPATH=\$PATH:~/.local/bin" >> ~/.bashrc
+wget -P ~/.local/lib https://bootstrap.pypa.io/get-pip.py; python ~/.local/lib/get-pip.py --user
+printf "\nPATH=\$PATH:~/.local/bin/" >> ~/.bashrc
 source ~/.bashrc
 pip install pytest --user  
